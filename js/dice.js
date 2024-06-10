bb9f24580a51db1f5842d98d32ecebfd569f1fb4
Cameron White <cameronwhite4121@gmail.com>
2024-06-09 17:56:53 -0700

    Added compiler options

---
+++ js/dice.js
@@ -0,0 +1,28 @@
+function generateRandomValue(minValue, maxValue) {
+    var random = Math.random();
+    return random;
+}
+function changePlayers() {
+    let currentPlayerName = document.getElementById("current").innerText;
+    let player1Name = document.getElementById("player1").value;
+    let player2Name = document.getElementById("player2").value;
+}
+window.onload = function () {
+    let newGameBtn = document.getElementById("new_game");
+    newGameBtn.onclick = createNewGame;
+    document.getElementById("roll").onclick = rollDie;
+    document.getElementById("hold").onclick = holdDie;
+};
+function createNewGame() {
+    document.getElementById("turn").classList.add("open");
+    document.getElementById("total").value = "0";
+    document.getElementById("player1").setAttribute("disabled", "disabled");
+    document.getElementById("player2").setAttribute("disabled", "disabled");
+    changePlayers();
+}
+function rollDie() {
+    let currTotal = parseInt(document.getElementById("total").value);
+}
+function holdDie() {
+    changePlayers();
+}
